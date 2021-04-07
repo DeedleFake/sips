b994c0ae5b1af4e608b54c1ba15ec0b59181b188
DeedleFake <deedlefake@users.noreply.github.com>
2021-04-06 23:43:01 -0400

    ipfsapi: fix some bugs and partially implement `pin ls`

--- ipfsapi/client.go
+++ ipfsapi/client.go
@@ -1,18 +1,24 @@
 package ipfsapi
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
 
 // Client is a client for the IPFS HTTP API.
 type Client struct {
-	c    *http.Client
-	base string
+	client *http.Client
+	base   string
 }
 
 // NewClient returns a new Client created with the given options.
 func NewClient(options ...ClientOption) *Client {
 	c := Client{
-		c:    http.DefaultClient,
-		base: "http://localhost:8080",
+		client: http.DefaultClient,
+		base:   "http://localhost:8080",
 	}
 	for _, option := range options {
 		option(&c)
@@ -21,20 +27,61 @@ func NewClient(options ...ClientOption) *Client {
 	return &c
 }
 
+func (c *Client) post(data interface{}, endpoint string, args url.Values) error {
+	url := c.base + "/" + endpoint + "?" + args.Encode()
+
+	rsp, err := c.client.Post(url, "application/json", nil)
+	if err != nil {
+		return fmt.Errorf("post to %q: %w", endpoint, err)
+	}
+	defer rsp.Body.Close()
+
+	buf, err := io.ReadAll(rsp.Body)
+	if err != nil {
+		return fmt.Errorf("read response: %w", err)
+	}
+
+	err = json.Unmarshal(buf, data)
+	if err != nil {
+		return fmt.Errorf("unmarshal response: %w", err)
+	}
+
+	return nil
+}
+
+// PinLS gets information about pins from the node.
+func (c *Client) PinLS(pintype PinType, cids ...string) ([]PinLS, error) {
+	var data []PinLS
+	err := c.post(&data, "/api/v0/pin/ls", url.Values{
+		"type": []string{string(pintype)},
+		"arg":  cids,
+	})
+	return data, err
+}
+
 type ClientOption func(*Client)
 
 // WithHTTPClient uses the given http.Client instead of
 // http.DefaultClient.
 func WithHTTPClient(client *http.Client) ClientOption {
 	return func(c *Client) {
-		c.c = client
+		c.client = client
 	}
 }
 
 // WithBaseURL sets the base URL for accessing the API. The default
-// is "http://localhost:8080".
+// is "http://localhost:5001".
 func WithBaseURL(base string) ClientOption {
 	return func(c *Client) {
 		c.base = base
 	}
 }
+
+type PinType string
+
+const (
+	Direct    PinType = "direct"
+	Indirect  PinType = "indirect"
+	Recursive PinType = "recursive"
+	All       PinType = "all"
+)

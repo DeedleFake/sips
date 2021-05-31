c862963939b6a65969ebe3b2e3c3b1b8c3a2b26a
DeedleFake <deedlefake@users.noreply.github.com>
2021-05-31 00:35:31 -0400

    cmd/sips: try to authenticate users when adding pins

--- cmd/sips/pinhandler.go
+++ cmd/sips/pinhandler.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/DeedleFake/sips"
+	"github.com/DeedleFake/sips/dbutil"
+	"github.com/DeedleFake/sips/internal/log"
 	"github.com/DeedleFake/sips/ipfsapi"
 	"go.etcd.io/bbolt"
+)
+
+var (
+	ErrNoToken = errors.New("no token")
 )
 
 type PinHandler struct {
@@ -18,6 +26,20 @@ func (h PinHandler) Pins(ctx context.Context, query sips.PinQuery) ([]sips.PinSt
 }
 
 func (h PinHandler) AddPin(ctx context.Context, pin sips.Pin) (sips.PinStatus, error) {
+	tok, ok := sips.Token(ctx)
+	if !ok {
+		return sips.PinStatus{}, ErrNoToken
+	}
+	user, err := dbutil.GetUserForToken(h.DB, tok)
+	if err != nil {
+		return sips.PinStatus{}, AuthError{
+			Token: tok,
+			Err:   err,
+		}
+	}
+
+	log.Infof("authenticated user %q with token %q", user, tok)
+
 	panic("Not implemented.")
 }
 
@@ -31,4 +53,13 @@ func (h PinHandler) UpdatePin(ctx context.Context, requestID string, pin sips.Pi
 
 func (h PinHandler) DeletePin(ctx context.Context, requestID string) error {
 	panic("Not implemented.")
+}
+
+type AuthError struct {
+	Token string
+	Err   error
+}
+
+func (err AuthError) Error() string {
+	return fmt.Sprintf("authenticate token %q: %v", err.Token, err.Err)
 }

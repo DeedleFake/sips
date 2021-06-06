669f111e7e57bc48618a125d54cbd590f016e976
DeedleFake <deedlefake@users.noreply.github.com>
2021-06-06 15:49:26 -0400

    cmd/sipsctl: begin implementing `pins sync`

--- cmd/sipsctl/cmd/pins.go
+++ cmd/sipsctl/cmd/pins.go
@@ -9,7 +9,7 @@ import (
 
 var pinsCmd = &cobra.Command{
 	Use:   "pins <subcommand>",
-	Short: "manually administrate pins",
+	Short: "administrate pins",
 	Long:  `Adds, removes, and lists pins without needing to use the HTTP API.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return cmd.Help()
@@ -41,5 +41,27 @@ func init() {
 		},
 	}
 
-	pinsCmd.AddCommand(listCmd)
+	var syncFlags struct {
+		Delete bool
+	}
+	syncCmd := &cobra.Command{
+		Use:   "sync",
+		Short: "synchronize pins to IPFS",
+		Long:  `Synchronizes pin status to actual IPFS pins.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			db, err := dbs.Open(rootFlags.DBPath)
+			if err != nil {
+				return fmt.Errorf("open database: %w", err)
+			}
+			defer db.Close()
+
+			panic("Not implemented.")
+		},
+	}
+	syncCmd.Flags().BoolFlag(&syncFlags.Delete, "delete", false, "delete missing pins from IPFS")
+
+	pinsCmd.AddCommand(
+		listCmd,
+		syncCmd,
+	)
 }

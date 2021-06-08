945b6cc2dc1f7502e2f60504eaf99590a890af02
DeedleFake <deedlefake@users.noreply.github.com>
2021-06-07 22:33:05 -0400

    cmd/sips: partially implement getting a list of pins

--- cmd/sips/pinhandler.go
+++ cmd/sips/pinhandler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strconv"
 	"time"
 
@@ -24,12 +25,47 @@ type PinHandler struct {
 }
 
 func (h PinHandler) Pins(ctx context.Context, query sips.PinQuery) ([]sips.PinStatus, error) {
-	//tokID, ok := sips.Token(ctx)
-	//if !ok {
-	//	return nil, ErrNoToken
-	//}
+	tokID, ok := sips.Token(ctx)
+	if !ok {
+		return nil, ErrNoToken
+	}
 
-	panic("Not implemented.")
+	tx, err := h.DB.Begin(false)
+	if err != nil {
+		log.Errorf("begin transaction: %v", err)
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	user, err := auth(tx, tokID)
+	if err != nil {
+		log.Errorf("authenticate with %v: %v", tokID, err)
+		return nil, err
+	}
+
+	// TODO: Filter things correctly using the query.
+
+	var dbpins []dbs.Pin
+	err = tx.Find("User", user.ID, &dbpins)
+	if err != nil {
+		log.Errorf("find pins for %v: %v", user.Name, err)
+		return nil, err
+	}
+
+	pins := make([]sips.PinStatus, 0, len(dbpins))
+	for _, pin := range dbpins {
+		pins = append(pins, sips.PinStatus{
+			RequestID: strconv.FormatUint(pin.ID, 16),
+			Status:    sips.Pinned, // TODO: Handle this properly.
+			Created:   pin.Created,
+			Pin: sips.Pin{
+				CID:  pin.CID,
+				Name: pin.Name,
+			},
+		})
+	}
+
+	return pins, nil
 }
 
 func (h PinHandler) AddPin(ctx context.Context, pin sips.Pin) (sips.PinStatus, error) {
@@ -45,17 +81,9 @@ func (h PinHandler) AddPin(ctx context.Context, pin sips.Pin) (sips.PinStatus, e
 	}
 	defer tx.Rollback()
 
-	var tok dbs.Token
-	err = tx.One("ID", tokID, &tok)
+	user, err := auth(tx, tokID)
 	if err != nil {
-		log.Errorf("find token %q: %v", tokID, err)
-		return sips.PinStatus{}, err
-	}
-
-	var user dbs.User
-	err = tx.One("ID", tok.User, &user)
-	if err != nil {
-		log.Errorf("find user %q: %v", tok.User, err)
+		log.Errorf("authenticate with %v: %v", tokID, err)
 		return sips.PinStatus{}, err
 	}
 
@@ -108,4 +136,19 @@ func (h PinHandler) UpdatePin(ctx context.Context, requestID string, pin sips.Pi
 
 func (h PinHandler) DeletePin(ctx context.Context, requestID string) error {
 	panic("Not implemented.")
+}
+
+func auth(db storm.Node, tokID string) (user dbs.User, err error) {
+	var tok dbs.Token
+	err = db.One("ID", tokID, &tok)
+	if err != nil {
+		return dbs.User{}, fmt.Errorf("find token: %w", err)
+	}
+
+	err = db.One("ID", tok.User, &user)
+	if err != nil {
+		return dbs.User{}, fmt.Errorf("find user: %w", err)
+	}
+
+	return user, nil
 }

f5e8db2f442eaa4d7679a3743a558c4f36f3a10b
DeedleFake <deedlefake@users.noreply.github.com>
2021-06-09 17:09:30 -0400

    cmd/sipsctl: add `users rm`

--- cmd/sipsctl/cmd/users.go
+++ cmd/sipsctl/cmd/users.go
@@ -81,8 +81,46 @@ func init() {
 		},
 	}
 
+	rmCmd := &cobra.Command{
+		Use:   "rm <names...>",
+		Short: "remove users from the database",
+		Args:  cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			db, err := dbs.Open(rootFlags.DBPath)
+			if err != nil {
+				return fmt.Errorf("open database: %w", err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin(true)
+			if err != nil {
+				return fmt.Errorf("begin transaction: %w", err)
+			}
+			defer tx.Rollback()
+
+			for _, arg := range args {
+				// TODO: Add the ability to also delete tokens and/or pins for
+				// each user.
+
+				var user dbs.User
+				err = tx.One("Name", arg, &user)
+				if err != nil {
+					return fmt.Errorf("find user %q: %w", arg, err)
+				}
+
+				err = tx.DeleteStruct(&user)
+				if err != nil {
+					return fmt.Errorf("delete user %q: %w", arg, err)
+				}
+			}
+
+			return tx.Commit()
+		},
+	}
+
 	usersCmd.AddCommand(
 		addCmd,
 		listCmd,
+		rmCmd,
 	)
 }

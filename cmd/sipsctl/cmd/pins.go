538e8bae8571b8ffc42a5175f42cc3cf50d398b1
DeedleFake <deedlefake@users.noreply.github.com>
2021-06-09 16:44:08 -0400

    cmd/sipsctl: add `pins rm`

--- cmd/sipsctl/cmd/pins.go
+++ cmd/sipsctl/cmd/pins.go
@@ -41,7 +41,51 @@ func init() {
 		},
 	}
 
+	var rmFlags struct {
+		Force bool
+	}
+	rmCmd := &cobra.Command{
+		Use:   "rm <names...>",
+		Short: "remove pins from the database",
+		Args:  cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			db, err := dbs.Open(rootFlags.DBPath)
+			if err != nil {
+				return fmt.Errorf("open database: %w", err)
+			}
+			defer db.Close()
+
+			tx, err := db.Begin(true)
+			if err != nil {
+				return fmt.Errorf("begin transaction: %w", err)
+			}
+			defer tx.Rollback()
+
+			for _, arg := range args {
+				var pins []dbs.Pin
+				err = db.Find("Name", arg, &pins)
+				if err != nil {
+					return fmt.Errorf("get pins: %w", err)
+				}
+				if (len(pins) > 1) && !rmFlags.Force {
+					return fmt.Errorf("found %v pins but no --force flag given", len(pins))
+				}
+
+				for _, pin := range pins {
+					err = tx.DeleteStruct(&pin)
+					if err != nil {
+						return fmt.Errorf("delete pin %v: %w", pin.ID, err)
+					}
+				}
+			}
+
+			return tx.Commit()
+		},
+	}
+	rmCmd.Flags().BoolVar(&rmFlags.Force, "force", false, "allow deletion of multiple matching pins per name")
+
 	pinsCmd.AddCommand(
 		listCmd,
+		rmCmd,
 	)
 }

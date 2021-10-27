48011724a6b8e42937ade9fb37409b34b6d08575
DeedleFake <deedlefake@users.noreply.github.com>
2021-10-26 20:36:23 -0400

    db: add `Open()` function

--- db/db.go
+++ db/db.go
@@ -8,9 +8,10 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// OpenAndMigrate opens the database and performs an auto-migration on it.
+// OpenAndMigrate opens the database and performs an auto-migration on
+// it.
 func OpenAndMigrate(ctx context.Context, driver, source string, opts ...ent.Option) (*ent.Client, error) {
-	entc, err := ent.Open(driver, source, opts...)
+	entc, err := Open(driver, source, opts...)
 	if err != nil {
 		return nil, fmt.Errorf("open database: %w", err)
 	}
@@ -21,4 +22,11 @@ func OpenAndMigrate(ctx context.Context, driver, source string, opts ...ent.Opti
 	}
 
 	return entc, nil
+}
+
+// Open opens the database. It is exactly equivalent to calling
+// ent.Open(), but it is preferred so that this package, and its
+// dependencies, are always imported.
+func Open(driver, source string, opts ...ent.Option) (*ent.Client, error) {
+	return ent.Open(driver, source, opts...)
 }
